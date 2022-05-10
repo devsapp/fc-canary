4c7d18d6e31f8166d5553a62d451a7556afccf04
长淇 <linanxuan.lnx@alibaba-inc.com>
2022-05-10 19:05:53 +0800

    change function

--- example/singleFunc/code/index.js
+++ example/singleFunc/code/index.js
@@ -1,10 +1,3 @@
-// exports.handler = function(event, context, callback) {
-//     callback(null, 'hello world');
-// };
-var getRawBody = require('raw-body');
-var getFormBody = require('body/form');
-var body = require('body');
-
 
 /*
 To enable the initializer feature (https://help.aliyun.com/document_detail/156876.html)
@@ -15,37 +8,8 @@ exports.initializer = (context, callback) => {
 };
 */
 
-exports.handler = (req, resp, context) => {
-  console.log('hello world');
-
-  var params = {
-    path: req.path,
-    queries: req.queries,
-    headers: req.headers,
-    method : req.method,
-    requestURI : req.url,
-    clientIP : req.clientIP,
-  }
-
-  getRawBody(req, function(err, body) {
-    for (var key in req.queries) {
-      var value = req.queries[key];
-      resp.setHeader(key, value);
-    }
-    resp.setHeader('Content-Type', 'application/json');
-    params.body = body.toString();
-    resp.send(JSON.stringify(params, null, '    '));
-  });
-
-  /*
-  getFormBody(req, function(err, formBody) {
-      for (var key in req.queries) {
-        var value = req.queries[key];
-        resp.setHeader(key, value);
-      }
-      params.body = formBody;
-      console.log(formBody);
-      resp.send(JSON.stringify(params));
-  });
-  */
+exports.handler = (req, resp) => {
+  console.log("receive body: ", req.body.toString());
+  resp.setHeader("Content-Type", "text/plain");
+  resp.send('<h1>test1</h1>');
 }
