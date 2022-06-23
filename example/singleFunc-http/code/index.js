c828f78e4f209dfbf01d3c897cc9f5cb20b9f76b
长淇 <linanxuan.lnx@alibaba-inc.com>
2022-06-23 15:58:45 +0800

    update example

--- example/singleFunc-http/code/index.js
+++ example/singleFunc-http/code/index.js
@@ -1,15 +1,6 @@
-
-/*
-To enable the initializer feature (https://help.aliyun.com/document_detail/156876.html)
-please implement the initializer function as below：
-exports.initializer = (context, callback) => {
-  console.log('initializing');
-  callback(null, '');
-};
-*/
-
 exports.handler = (req, resp) => {
   console.log("receive body: ", req.body.toString());
   resp.setHeader("Content-Type", "text/plain");
-  resp.send('<h1>test1</h1>');
+  // throw new Error("My error");
+  resp.send('<h1>hello world</h1>');
 }
